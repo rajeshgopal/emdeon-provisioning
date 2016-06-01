d456148d37b76ee58f023580577e25f29cd70c81
Rajesh Gopal <rajeshsmyl@gmail.com>
2016-06-01 17:54:01 +0530

    removed dependency modules

--- modules/galera/examples/site.pp
+++
@@ -1,7 +0,0 @@
-node default {
-  class { 'galera':
-    galera_servers => ['192.168.99.101','192.168.99.102'],
-    galera_master  => 'control1.domain.name',
-    vendor_type    => 'mariadb'
-  }
-}
