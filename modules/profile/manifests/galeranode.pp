3e943a72620ca86df7c5de6961d15b8969a2d4c2
rajeshgopal <rajeshsmyl@gmail.com>
2016-06-14 18:37:09 +0530

    Update galeranode.pp

--- modules/profile/manifests/galeranode.pp
+++ modules/profile/manifests/galeranode.pp
@@ -72,12 +72,7 @@ class { '::galera':
   }
 }
 
-#package {'mariadb-libs':
-# ensure => absent,
-# before => Class['galera']
-#}
-
-Yumrepo['galera']-> Class['selinux']->File['/usr/lib/mysql/wsrep_notify.sh']->Class['::galera']
+Class['::profile::consulagent']-> Yumrepo['galera']-> Class['selinux']->File['/usr/lib/mysql/wsrep_notify.sh']->Class['::galera']
 
 
 }
