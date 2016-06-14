bb4da611eee1bc2a15aca663aebfa03f527c25c3
rajeshgopal <rajeshsmyl@gmail.com>
2016-06-14 19:58:24 +0530

    Update base.pp

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -14,4 +14,22 @@ file { '/opt/python/shell_comm.py':
     mode    => '0644',
   }
 
+## To setup masterkey and retrieve galera IPs
+$consulmaster = '10.0.0.15'
+$galeraips = generate("/bin/bash","-c", "python /opt/python/check_node_key.py $consulmaster")
+
+#Retrieves galera nodes IP
+if $galeraips =~ /(.*\n)+(?!(.*\n))/ {
+ if $1 == '' {
+   $galeranodes = [$::fqdn]
+   }
+ else {
+   $galeranodes = [$1]
+   }
+ }
+#retrieves galera Master IP
+if $galeraips =~ /(.*)/ {
+$galeramaster = $1
+}
+
 }
