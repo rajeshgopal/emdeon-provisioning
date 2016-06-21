bd75fe52d9122ad0b4a86b83228bf1974f81c852
rajeshgopal <rajeshsmyl@gmail.com>
2016-06-21 18:09:55 +0530

    Update consulagent.pp

--- modules/profile/manifests/consulagent.pp
+++ modules/profile/manifests/consulagent.pp
@@ -6,7 +6,7 @@ class { '::consul':
    'data_dir'   => '/opt/consul',
    'datacenter' => 'aws-east',
    'log_level'  => 'INFO',
-   'retry_join' => ['10.0.0.246'],
+   'retry_join' => ['10.0.0.141'],
    'client_addr' => $::ipaddress,
  },
  version => '0.6.4',
